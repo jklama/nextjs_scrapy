7714eb8c05099ad10eece991b005de531e001fd0
jk_lama <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>
2023-10-19 07:38:26 +0545

    Added all other functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () =>{
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
-    <section className="px-6 md:px-20 py-24">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center"> 
             <p className="small-text">
               Smart Shopping Starts Here:
-              <Image
+              <Image 
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
                 width={16}
@@ -27,7 +31,7 @@ const Home = () =>{
               Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
 
-            <Searchbar /> 
+            <Searchbar />
           </div>
 
           <HeroCarousel />
@@ -38,10 +42,9 @@ const Home = () =>{
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          hello
-          {/* {allProducts?.map((product) => (
+          {allProducts?.map((product) => (
             <ProductCard key={product._id} product={product} />
-          ))} */}
+          ))}
         </div>
       </section>
     </>
